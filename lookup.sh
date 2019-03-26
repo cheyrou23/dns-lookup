d2c0b4fb1b40d3af78b27e38f7a94dbcfaefdde4
Nick W <cheyrou23@gmail.com>
2019-03-26 11:23:50 +0000

    Update to lookup.sh to use for whois only
    Removed other lookups and added a little sleep.

--- lookup.sh
+++ lookup.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 # Put all the domain names in domains.txt, one per line
-# then run in the terminal: bash domains.sh
-# this will print each domain in the terminal as it looks it up
-# The result csv will contains the domain, IP & Nameservers in each column
+# then run in the terminal: bash lookup.sh
+# this will print each domain and domain owner in the terminal as it looks it up
+# The result csv will contains the domain and domain owner separated by a colon
 
 # Give each column the relevant header titles
-echo "Domain Name,IP Address,Nameserver,Nameserver,Nameserver,Nameserver,Nameserver" > domains.csv
+echo "Domain Owner:Domain Name" > domains.csv
 
 while read domain
 do
-  # Get IP address defined in domain's root A-record
-  ipaddress=`dig $domain +short`
+# Use the line below to extract any information from whois
+# Remove or change --host=whois.ja.net to use a different whois server
+ns=`whois --host=whois.ja.net $domain | grep -A1 "Domain Owner" | tr '\n' ':' | cut -f 2`
 
-  # Get list of all nameservers
-  ns=`dig ns $domain +short| sort | tr '\n' ','`
+# Prints the domain owner and domain name in the terminal
+echo "$ns$domain"
+   
+# Prints the domain owner and domain name into the CSV file
+echo "$ns$domain" >> domains.csv
 
-  # Use the line below to extract any information from whois
-  # ns=`whois $domain | grep "Name Server" | cut -d ":" -f 2 |  sed 's/ //' | sed -e :a -e '$!N;s/ \n/,/;ta'`
-
-  echo "$domain"
-
-  # Uncomment the following lines to output the information directly into the terminal
-  # echo "IP Address:  $ipaddress"
-  # echo "Nameservers: $ns"
-  # echo " "
-  
-  # Prints all the values fetched into the CSV file
-  echo -e "$domain,$ipaddress,$ns" >> domains.csv
+# Waits 25 seconds so as not to hit the rate limiter on the whois server
+sleep 25
 
 # Defines the text file from which to read domain names
 done < domains.txt
